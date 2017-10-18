6ee94a22eb124ff23a878af740d8f7aada1c2cf1
Claireywp <1377877512ywp@sina.com>
2017-10-18 21:19:00 +0800

    ado.net test

---
+++ programing/Default.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true"  CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title>无标题页</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="Label1" runat="server" Text="Student_id"></asp:Label>
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <asp:Label ID="Label2" runat="server" Text="name "></asp:Label>
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <asp:Label ID="Label3" runat="server" Text="Class_id"></asp:Label>
+        <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ado_TestConnectionString %>"
+            SelectCommand="SELECT DISTINCT id, memo, classid, name FROM info" OnSelecting="SqlDataSource1_Selecting"></asp:SqlDataSource>
+    
+    </div>
+        &nbsp;
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataSourceID="SqlDataSource1"
+            Height="145px">
+            <Columns>
+                <asp:BoundField DataField="id" HeaderText="id" SortExpression="id" />
+                <asp:BoundField DataField="name" HeaderText="name" SortExpression="name" />
+                <asp:BoundField DataField="classid" HeaderText="classid" SortExpression="classid" />
+                <asp:BoundField DataField="memo" HeaderText="memo" SortExpression="memo" />
+            </Columns>
+        </asp:GridView>
+        <asp:GridView ID="GridView3" runat="server" DataSourceID="SqlDataSource1">
+        </asp:GridView>
+        <asp:GridView ID="GridView2" runat="server">
+        </asp:GridView>
+        &nbsp;
+        &nbsp;&nbsp;
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Test1" />
+        <asp:Button ID="Button2" runat="server" Text="Test2" OnClick="Button2_Click" />
+        <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Test3" />
+    </form>
+</body>
+</html>
---
+++ programing/Default.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Data.SqlClient;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+       
+        DataSourceSelectArguments ar = new DataSourceSelectArguments();
+        DataView dv = (DataView)SqlDataSource1.Select(ar);      //将“SqlDataSource1”替换为所需名
+
+        //开始赋值，只有一行。Rows[0]代表取第一行
+        this.TextBox1.Text  = dv.Table.Rows[0]["id"].ToString();
+        this.TextBox2.Text = dv.Table.Rows[0]["name"].ToString();
+
+    }
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        string strconn = ConfigurationManager.ConnectionStrings["ado_TestConnectionString"].ConnectionString;
+        SqlConnection cn = new SqlConnection(strconn);
+        cn.Open();//打开这个连接
+
+        string mysql = "select * from info where classid=30";
+        SqlCommand cm = new SqlCommand(mysql, cn);
+        SqlDataReader dr = cm.ExecuteReader();//执行这个查询，将结果放到DataReader对象
+
+        if (dr.Read())   //如果存在
+        {
+           this.TextBox3.Text = dr.GetValue(2).ToString();
+        }
+        cn.Close();
+    }
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        //建立连接       
+        string strconn = ConfigurationManager.ConnectionStrings["ado_TestConnectionString"].ConnectionString;
+        SqlConnection cn = new SqlConnection(strconn);
+        cn.Open();
+
+        //sql语句
+        string mysql = "select * from info where classid=10";
+
+        SqlDataAdapter ad = new SqlDataAdapter(mysql, cn);//新建一个adapter,用以获取数据
+        DataSet ds = new DataSet();//新建一个dataset，用以获取adapter中的值
+        ad.Fill(ds, "info");//为dataset赋值，必须指定adapter中某个表。
+        GridView2.DataSource = ds;//将dataset中的值绑定给gridview，展示到客户端。
+        GridView2.DataBind();
+        cn.Close();
+    }
+    protected void SqlDataSource1_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
+    {
+
+    }
+}
